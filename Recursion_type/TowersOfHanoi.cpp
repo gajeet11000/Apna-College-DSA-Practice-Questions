e0ec7c241bd0ad44dee2a5d554f79595f71b4197
G Ajeet <gajeet11000@gmail.com>
2021-06-09 10:16:39 +0530

    modified TowersOfHanoi to a clever code

--- Recursion_type/TowersOfHanoi.cpp
+++ Recursion_type/TowersOfHanoi.cpp
@@ -13,19 +13,23 @@
 
 using namespace std;
 
-void towersOfHanoi(int dics, char from, char spare, char to)
+int step{1};
+
+void towersOfHanoi(int dics, int from, int to)
 {
+	int temp = 6 - from -to;
+
 	if(dics == 1)
 	{
-		cout << "Moving from " << from << " to " << to << "." << endl;
+		cout << step++ << ". Moving from " << from << " to " << to << "." << endl;
 	}
 	else
 	{
-		towersOfHanoi(dics-1, from, to, spare);
+		towersOfHanoi(dics-1, from, temp);
 
-		cout << "Moving from " << from << " to " << to << "." << endl;
+		cout << step++ << ". Moving from " << from << " to " << to << "." << endl;
 
-		towersOfHanoi(dics-1, spare, from, to);
+		towersOfHanoi(dics-1, temp, to);
 	}
 }
 
@@ -35,7 +39,7 @@ int main()
 	int dics{};
 	cin >> dics;
 
-	towersOfHanoi(dics, 'A', 'B', 'C');
+	towersOfHanoi(dics, 1, 3);
 	return 0;
 }
 
