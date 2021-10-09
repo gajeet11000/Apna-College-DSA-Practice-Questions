29119f2e9f439c6f75dc55fea3dc5413e1523f79
G Ajeet <gajeet11000@gmail.com>
2021-10-09 21:03:49 +0530

    complete Wave Sort

---
+++ Sorting_algos/WaveSort.cpp
@@ -0,0 +1,72 @@
+#include <unordered_map>
+#include <string>
+#include <map>
+#include <cctype>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+#include <string>
+
+#define ll long long
+
+using namespace std;
+
+void printArray(int array[], int size)
+{
+	for(int i=0 ; i<size ; i++)
+	{
+		cout << array[i] << " ";
+	}
+}
+
+void inputArray(int array[], int size)
+{
+	for(int i=0 ; i<size ; i++)
+	{
+		cin >> array[i];
+	}
+}
+
+void swap(int array[], int i, int j)
+{
+	int temp{array[i]};
+	array[i] = array[j];
+	array[j] = temp;
+}
+
+void waveSort(int array[], int size)
+{
+	for(int i=1 ; i<size-1 ; i+=2)
+	{
+		if(array[i] > array[i-1])
+		{
+			swap(array, i, i-1);
+		}
+
+		if(array[i] > array[i+1] && i < size-1)
+		{
+			swap(array, i, i+1);
+		}
+	}
+}
+
+int main()
+{
+	cout << "Enter the size of the array: ";
+	int size{};
+	cin >> size;
+
+	int array[size];
+
+	cout << "\nEnter the array elements:-" << endl;
+	inputArray(array, size);
+
+	waveSort(array, size);
+
+	cout << "\nSorted array in the wave form:- "<< endl;
+	printArray(array, size);
+
+	return 0;
+}
