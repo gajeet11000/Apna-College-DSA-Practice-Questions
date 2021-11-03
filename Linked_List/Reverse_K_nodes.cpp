e4daa59e1601e787667f7d73d133e837e3ff4ce5
G Ajeet <gajeet11000@gmail.com>
2021-11-03 23:20:55 +0530

    complete my version of reversing K nodes

--- Linked_List/Reverse_K_nodes.cpp
+++ Linked_List/Reverse_K_nodes.cpp
@@ -13,8 +13,94 @@
 
 using namespace std;
 
+class Node
+{
+public:
+	int data;
+	Node *next;
+
+	Node(int val)
+	{
+		data = val;
+		next = NULL;
+	}
+};
+
+void insertAtTail(Node* &head, int data)
+{
+	Node* new_node = new Node(data);
+
+	if(head == NULL)
+	{
+		head = new_node;
+	}
+	else
+	{
+		Node* temp = head;
+
+		while(temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+
+		temp->next = new_node;
+	}
+}
+
+Node* reverseKNodes(Node *&head, int k)
+{
+
+	if(head == NULL || head->next == NULL || k == 0)
+	{
+		return head;
+	}
+
+	Node* previous_ptr = NULL;
+	Node* current_ptr = head;
+	Node* next_ptr = NULL;
+
+	int k_temp = k;
+
+	while(k_temp-- && current_ptr != NULL)
+	{
+		next_ptr = current_ptr->next;
+		current_ptr->next = previous_ptr;
+
+		previous_ptr = current_ptr;
+		current_ptr = next_ptr;
+	}
+
+	reverseKNodes(next_ptr, k);
+
+	head->next = next_ptr;
+	head = previous_ptr;
+	return head;
+}
+
+void displayList(Node *head)
+{
+	while(head != NULL)
+	{
+		cout << head->data << " ";
+		head = head->next;
+	}
+	cout << endl;
+}
+
 int main()
 {
-	
+	Node* head = NULL;
+
+	insertAtTail(head, 1);
+	insertAtTail(head, 2);
+	insertAtTail(head, 3);     
+	insertAtTail(head, 4);
+	insertAtTail(head, 5);
+	insertAtTail(head, 6);
+
+	displayList(head);
+	Node *new_head = reverseKNodes(head);
+	displayList(new_head);
+
 	return 0;
 }
